2a01369ef72c451ad8834702a140f0d4642bc343
Samuel Sleight <samuel.sleight@gmail.com>
2014-01-26 21:12:49 +0000

    Removed a comment

--- test.hs
+++ test.hs
@@ -13,15 +13,6 @@ keyPressed evt game = case code evt of
 onResize :: SFEvent -> Game -> IO ()
 onResize evt = teaResizeView (width evt) (height evt)
 
---main :: IO ()
---main = do
-	--game <- teaInit
-	--game <- teaBindEvent game TeaClosed exitGame
-	--game <- teaBindEvent game TeaKeyPressed keyPressed
-	--game <- teaBindEvent game TeaResized onResize
-	--game <- teaRun game
-	--teaDestroy game
-
 main :: IO ()
 main = teaInit
 	>>= teaBindEvent TeaClosed exitGame
